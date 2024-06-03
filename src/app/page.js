da89add88ad5d706f5bf2e9e63b83582c23c9a45
neha <nehakashyap1011@gmail.com>
2024-06-03 23:29:51 +0530

    made client component using use client

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,8 @@
+"use client";
 import Image from "next/image";
+import { useState } from "react";
 const home = () => {
+  const [value, setValue] = useState(false);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <User name="neha" />
