4a08b4c6ddf5dac42c7f99811ea819ee75f290e5
neha <nehakashyap1011@gmail.com>
2024-06-04 08:21:03 +0530

    Applied concept of dynamic route and catched all route

---
+++ src/app/products/[...product-review]/page.js
@@ -0,0 +1,3 @@
+export default function productReview() {
+  return <h1>This is product Review page and catch all routes</h1>;
+}
---
+++ src/app/products/[details]/page.js
@@ -0,0 +1,3 @@
+export default function productDetails() {
+  return <h1>This is product details page</h1>;
+}
