5cb9d4e44aa7733ea1397018655daa6501b98287
neha <nehakashyap1011@gmail.com>
2024-06-05 08:17:11 +0530

    navigated in client component and server component

--- src/app/account/page.js
+++ src/app/account/page.js
@@ -1,3 +1,7 @@
+import { redirect } from "next/navigation";
+
 export default function Account() {
+  const userProfile = null;
+  if (userProfile == null) redirect("profile");
   return <h1> Account page</h1>;
 }
---
+++ src/app/profile/page.js
@@ -0,0 +1,3 @@
+export default function profilePage(){
+    return <h1> This is profile page</h1>
+}
